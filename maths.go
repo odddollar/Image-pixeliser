650d32daefb076e1e5ac70c1a3d0f537812bb965
Simon Eason <sie.eason@gmail.com>
2021-01-27 09:02:57 +0800

    fixed function to stop resizing perfectly divisible images

--- maths.go
+++ maths.go
@@ -4,22 +4,22 @@ func getClosestDimensions(desiredX, desiredY, width, height int) (int, int) {
 	var resizedWidth, resizedHeight int
 
 	// while i is less than the width
-	for i := 0; i < width; {
-		if i+desiredX < width { // check if can step up another level, step up by that level
-			i += desiredX
-		} else { // if not possible to step up another level, return current level
+	for i := 0; i <= width; {
+		if i+desiredX > width { // if not possible to step up another level, return current level
 			resizedWidth = i
 			break
+		} else { // check if can step up another level, step up by that level
+			i += desiredX
 		}
 	}
 
 	// do same as above but with height
-	for i := 0; i < height; {
-		if i+desiredY < height {
-			i += desiredY
-		} else {
+	for i := 0; i <= height; {
+		if i+desiredY > height {
 			resizedHeight = i
 			break
+		} else {
+			i += desiredY
 		}
 	}
 
