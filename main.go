326a923ff6a53a2f09f4e141b61b32f3fccac73d
Simon Eason <sie.eason@gmail.com>
2021-01-27 18:58:13 +0800

    added argument parser

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"github.com/akamensky/argparse"
 	"image"
 	"image/jpeg"
 	"image/png"
+	"os"
 )
 
 func main() {
@@ -11,12 +14,23 @@ func main() {
 	image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
 	image.RegisterFormat("fpg", "jpg", jpeg.Decode, jpeg.DecodeConfig)
 
+	// create argparser
+	parser := argparse.NewParser("Image pixeliser", "Converts a given image to a specified number of colour blocks on the x and y axes")
+	file := parser.String("f", "file", &argparse.Options{Required: true, Help: "The path of the image file to convert"})
+	width := parser.Int("x", "width", &argparse.Options{Required: true, Help: "The number of colour blocks to have along the x axis"})
+	height := parser.Int("y", "height", &argparse.Options{Required: true, Help: "The number of colour blocks to have along the y axis"})
+
+	// run argparser
+	if err := parser.Parse(os.Args); err != nil {
+		fmt.Println(parser.Usage(err))
+	}
+
 	// get number of desired colour blocks
-	desiredX := 8
-	desiredY := 8
+	desiredX := *width
+	desiredY := *height
 
 	// open image and get width and height
-	workingImage, workingImageWidth, workingImageHeight := readImage("testImage.jpg")
+	workingImage, workingImageWidth, workingImageHeight := readImage(*file)
 
 	// resize to temporary image of size that fits number of desired blocks closest to current width
 	newWidth, newHeight := getClosestDimensions(desiredX, desiredY, workingImageWidth, workingImageHeight)
