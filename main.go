5dc7b67ec686425bf7b8c47b8f5ebf8e6b185b91
Simon Eason <sie.eason@gmail.com>
2021-01-27 17:51:05 +0800

    added support for jpg input files

--- main.go
+++ main.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"image"
+	"image/jpeg"
 	"image/png"
 )
 
 func main() {
 	// create image decoder
 	image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
+	image.RegisterFormat("fpg", "jpg", jpeg.Decode, jpeg.DecodeConfig)
 
 	// get number of desired colour blocks
 	desiredX := 8
 	desiredY := 8
 
 	// open image and get width and height
-	workingImage, workingImageWidth, workingImageHeight := readImage("testImage.png")
+	workingImage, workingImageWidth, workingImageHeight := readImage("testImage.jpg")
 
 	// resize to temporary image of size that fits number of desired blocks closest to current width
 	newWidth, newHeight := getClosestDimensions(desiredX, desiredY, workingImageWidth, workingImageHeight)
