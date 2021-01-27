54e4ce032673001aaaa9f76da624d8495e02fec8
Simon Eason <sie.eason@gmail.com>
2021-01-27 08:41:50 +0800

    major refactoring of project structure and functionality

---
+++ imageHandling.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"image"
+	"log"
+	"os"
+)
+
+func readImage(path string) (image.Image, int, int) {
+	// open file
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() { _ = file.Close() }()
+
+	// decode image to array
+	img, _, err := image.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// return image
+	return img, img.Bounds().Max.X, img.Bounds().Max.Y
+}
--- main.go
+++ main.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/png"
-	"log"
-	"os"
 )
 
-type Pixel struct {
-	r, g, b int
-}
-
 func main() {
-	// get number of desired colour blocks
+	// create image decoder
+	image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
 
-	// open image
+	// get number of desired colour blocks
+	desiredX := 8
+	desiredY := 8
+
+	// open image and get width and height
+	workingImage, workingImageWidth, workingImageHeight := readImage("testImage.png")
 
 	// resize to temporary image of size that fits number of desired blocks closest to current width
 
@@ -25,50 +24,4 @@ func main() {
 
 	// write each pixel to output file
 
-	image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
-
-	fmt.Println(readImagePixels(readImage("testImage.png")))
-}
-
-func readImagePixels(image image.Image) [][]Pixel {
-	// create pixel array
-	var pixels [][]Pixel
-
-	// get image bounds/size
-	width, height := image.Bounds().Max.X, image.Bounds().Max.Y
-
-	// iterate through height of image
-	for y := 0; y < height; y++ {
-		// create temporary row variable to store values of individual row
-		var row []Pixel
-		for x := 0; x < width; x++ {
-			row = append(row, rgbaToPixel(image.At(x, y).RGBA()))
-		}
-		pixels = append(pixels, row)
-	}
-
-	// return pixel array
-	return pixels
-}
-
-func readImage(path string) image.Image {
-	// open file
-	file, err := os.Open(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func() { _ = file.Close() }()
-
-	// decode image to array
-	img, _, err := image.Decode(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// return image
-	return img
-}
-
-func rgbaToPixel(r uint32, g uint32, b uint32, _ uint32) Pixel {
-	return Pixel{int(r / 257), int(g / 257), int(b / 257)}
 }
